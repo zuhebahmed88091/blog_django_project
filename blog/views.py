c6889966a9f8d6b0b8074b7fe6c3340da1093472
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-19 16:28:47 +0600

    feat:add the data which are created in python shell in database to our code(see views.py), fix:deleted ta dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
-
-posts = [
-    {
-        'author':'Zuheb Ahmed',
-        'title':'Blog post 1',
-        'content':'First post content',
-        'date_posted':'May 18, 2022'
-    },
-    {
-        'author':'Linas',
-        'title':'Blog post 2',
-        'content':'Second post content',
-        'date_posted':'June 6, 2022'
-    }
-]
-
 
 def home(request):
     context = {
-        'posts': posts
+        # 'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+#dummy data
+# posts = [
+#     {
+#         'author':'Zuheb Ahmed',
+#         'title':'Blog post 1',
+#         'content':'First post content',
+#         'date_posted':'May 18, 2022'
+#     },
+#     {
+#         'author':'Linas',
+#         'title':'Blog post 2',
+#         'content':'Second post content',
+#         'date_posted':'June 6, 2022'
+#     }
+# ]
+
