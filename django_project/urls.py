3b6a3aba7d4ac3c7e7ed0fe251cffb9b44e8bd5b
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-20 14:47:58 +0600

    update: updated the register function in views.py of users model to validate the created form feat: add a html template for user registration and validation and import flash messages and in views.py after that redirected to the blog-home which inherits from base.html update:updated base.html for flash messages

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("register/", user_view.register, name="register"),
     path("", include("blog.urls"))
 
 ]
