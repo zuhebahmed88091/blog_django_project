e750b8085e946896b34deae6219d6a17eadbd85f
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-22 14:54:16 +0600

    fix: in views.py of users changed messages and redirect page to login that means after creating an account user will see the login page. feat: created logout.html and it's function and the path was already set in urls.py of project

--- users/views.py
+++ users/views.py
@@ -10,8 +10,10 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            # messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your Account Has Been Created! You Are Now Able To Log In')
+            return redirect('login')
+            # return redirect('blog-home')
     else:
         # form = UserCreationForm()
         form = UserRegisterForm()
