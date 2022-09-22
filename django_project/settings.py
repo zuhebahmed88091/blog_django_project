4974761a050603aec5d66e387d7ac71d6c52213f
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-23 00:15:34 +0600

    feat: initial plan is when the user is logged in he can view his name in profile if not profile route is restricted. To do that firstly created a url for profile in urls.py path(profile/, user_view.profile, name=profile) like that and then created profile.html which shows username and in our navbar (base.html) if the user is looged in condition it would show profile tab. To restrict the profile while user is not logged in did import a from django.contrib.auth.decorators import login_required and before def profile @login_required is added and lastly while not logged in and user tries to access profile page it would redirect him to login page and for that used LOGIN_URL = 'login' in settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -124,3 +124,4 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -18,6 +19,10 @@ def register(request):
         # form = UserCreationForm()
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
 
 # messages.warning
