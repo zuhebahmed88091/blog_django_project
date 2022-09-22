917e646258eeee3c7123491ce9a384f5d665cffa
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-22 10:09:17 +0600

    update: in views.py of users commented out UserCreationForm and form = UserCreationForm(request.POST),form = UserCreationForm() instead we use form = UserRegisterForm(request.POST),form = UserRegisterForm() and import from .forms import UserRegisterForm and commented from django.contrib.auth.forms import UserCreationForm feat: to add email field use smart approach like created forms.py in users app then we import forms, User model and UserCreationModel and created UserRegisterForm which inherits UserCreationForm and then created and email field and to conncet with user model we use Meta class

--- users/views.py
+++ users/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        # form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        # form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
 
 
