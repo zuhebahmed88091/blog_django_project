afdcaa610d307bbaf515aea68c8a30873f53267e
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-22 09:06:55 +0600

    feat: to save user created form.save() in views.py

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
