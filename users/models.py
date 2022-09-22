fd40bcd0cffd2222615566fb53030c5f2de0dbfe
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-23 01:07:08 +0600

    feat: image function added(models.py) in profile

--- users/models.py
+++ users/models.py
@@ -3,3 +3,4 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
