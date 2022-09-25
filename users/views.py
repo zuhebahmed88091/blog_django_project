81612ab32b18491cc03f9470dd345d4dd06989a6
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-25 14:44:52 +0600

    a model form allows us to create a form that will work with a specific database model.In this case we want a form that will update our user model.

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == "POST":
@@ -22,7 +22,14 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request, 'users/profile.html', context)
 
 
 # messages.warning
