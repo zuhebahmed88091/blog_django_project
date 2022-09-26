81eb194e7d206b481f1472d571958896b06f0911
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-26 13:39:28 +0600

    feat: just like register we will check if this is POST route if it is,then we'll want to see if our forms are valid.If the forms are valid, then we will save that information. if request.method == POST: this is what will be run when we submit our form and possibly pass in new data.So we want to passin the post data into our forms so the instances will be same.To pass the post data u_form = UserUpdateForm(request.POST, instance=request.user), p_form = ProfileUpdateForm(request.POST, instance=request.user.profile). request.FILES in p_form to upload the image by user. This whole portion helps to update the profile and also update in the admin page.if u_form.is_valid() and p_form.is_valid(): u_form.save() p_form.save() messages.success(request, f'Your Account Has Been Updated!') return redirect('profile')

--- users/views.py
+++ users/views.py
@@ -22,8 +22,18 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your Account Has Been Updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form':u_form,
