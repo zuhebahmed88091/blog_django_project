85e725cbcfced2ad2e1391e3206f9810048915c4
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-26 11:23:58 +0600

    update: while updating the profile fields aren't filled with current user information. To do that in views.py have to do u_form = UserUpdateForm(instance=request.user) and p_form = ProfileUpdateForm(instance=request.user.profile)

--- users/views.py
+++ users/views.py
@@ -22,8 +22,8 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form':u_form,
