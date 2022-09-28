cf64a1cdd6ec37930a92a7416cf4427be026aa8d
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-28 11:20:18 +0600

    update: In blog we are going to look at using class-based  views in order to display, update and delete new post.In general we used function based views.Our url patterns are directed to certain view which are these functions and views then handle the logic for the routes and then render our templates. Now we are going to create class-based view for our home page. Our home page will be list view.At first import list view.Then we have to create class. We created PostListView class which inherits from ListView and it there we have to create a vaariable named model and this will tell our list view that what model to query in order to create the list. In this case we want it to be all of our posts (model = Post).To access this class based view we have to access it from blog urls.py. At first we have to import from .views import  PostListView.Now instead using views.home we will replace it with PostListView.as_view(). To access aour tempalte django automatically search for <app>/<model>_<viewtype>.html. But we have an existing template. So we will take a variable like template_name = 'blog/home.html'. Now we set the variable here within our list view like context_object_name = 'posts'.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 # from django.http import HttpResponse
 
@@ -8,6 +9,12 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
