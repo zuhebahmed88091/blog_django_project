128d652e6568707995b8ba16f0978185e01d705b
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-28 22:59:39 +0600

    feat: Every post needs to have an author. Now we want the author of the post to be the current logged in user. To do that we have to override the form valid method for create view and that will allow us to add the author before the form is submitted.In views.py at our PostCreateView we need to override the form valid method. So we will create def form_valid(self, form): and within here we can simply set the author on the form by saying form.instance.author = self.request.user, so basically we are saying that hey that form that you are trying to submit before you do that take that instance and set the author equal to the current logged in user. After doing that we can validate the form by return super().form_valid(form). Basically return super().form_valid(form) this line here is just running that form valid method on our parent class. This will set the author. If we run that we will get an error that will say an redirect procedure error. Basically it's telling us that it created the post successfully but it doesn't know where we want to be redirected to now. The way to tell django how to find the url of a model object is to create get absolute url method in our model that returns the path to any specific instance. In our blog's model.py we're going to be get the url of a particular route and in order to do this we need to use the reverse function. Reverse will simply return the full url to that route as a string. In this case we simply want to return the url as a string and let the view handle the redirect for us.First we have to import the reverse function. Then in our Post model we write def get_absolute_url(self): return reverse(post-detail) and we want to get is the path to the post-detail route and it needs a specific post with a primary key. So we write return reverse(post-detail, kwargs={pk: self.pk}).  So the instance of a specific post primary key. This is how we can create post and redirect it.

--- blog/views.py
+++ blog/views.py
@@ -27,6 +27,11 @@ class PostCreateView(CreateView):
     model = Post
     fields = ['title', 'content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
