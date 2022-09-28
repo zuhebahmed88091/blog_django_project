3854bcd7d5112c6712bbcc06c826da352332d2fa
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-28 12:31:13 +0600

    feat: We will create view for individual post. So this is going to be a detail view. We have to import DetailView and in urls.py import PostDetailView. Then we have to create a url pattern that contains a variable like path(post/<int:pk>/, PostDetailView.as_view(), name=post-detail). And now we will have to create a tempalte with django naming convention like <app>/<model>_<viewtype>.html. So the name will be post_detail.html. When we are dealing with detail views it expects the context of this template to be called object. So in this tempalte where there is post we will replace them with object. Also we will add <h2 class=article-title> {{ object.title }} </h2> instead of <h2><a class=article-title href=#>{{ post.title }}</a></h2> and also remove the for loop section.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import  PostListView
+from .views import  PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
     # path("", views.home, name="blog-home"),
     path("", PostListView.as_view(), name="blog-home"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("about/", views.about, name="blog-about")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 # from django.http import HttpResponse
 
@@ -15,6 +15,9 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
