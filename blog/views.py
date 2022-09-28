8c06b2089a84d74a1c2189f1712f8f627415b121
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-28 11:28:56 +0600

    fix: We want to see latest post at top. To fix this we have to change the order our query is making to the database. In our views.py in PostListView have to create ordering = ['-date_posted']. and in previous commit we had to change in our blog urls.py instead of another path using PostListView.as_view()

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'  #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-
+    ordering = ['-date_posted']
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
