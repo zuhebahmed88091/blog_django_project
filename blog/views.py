3498c5ddbd597bf0c1d8d5c8f8e1d479ce1e0084
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-29 09:21:01 +0600

    feat: We want to create a delete view for deleting post.First import DeleteView in views.py of blog. Then have to create a class class PostDeleteView(DeleteView): model = Post. Unlike detail view we want to require a ser to be logged in here and also require that the user is the author of the post in order to see the delete view. To do that we have to do class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView): model = Post def test_func(self): post = self.get_object() if self.request.user == post.author: return True else: return False. Now we need to do is add a path to our url pattern. So in urls.py we have to import PostDeleteView and add path(post/<int:pk>/delete, PostDeleteView.as_view(), name=post-delete). Now we will need a template and the template that this expects is just a form that asks if we're sure that we want to delete the post and if we submit the form then the post will be deleted. The naming convention of the template is post_confirm_delete.html and this is similar to post_form.html and we will remove {% load crispy_forms_tags %} and {{ form|crispy }}. We also change legend into Delete Post. Then we will add <h2>Are you sure you want to delete the post {{ object.title }}</h2> and <button class=btn btn-danger type=submit>Yes, Delete</button>. If user don't want to delete we have to add cancel link that takes them back to the post detail view. To do that we will have to add <a class=btn btn-secondary href={% url 'post-detail' object.id %}>Cancel</a>. After deletion we will have to redirect and need to provide a success url. So in our blog/views.py at our delete view we need to add a success url attribute and we'll set this to the home page. So after the model = Post we will add success_url = '/'. This is working now

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import Post
 # from django.http import HttpResponse
@@ -40,6 +41,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
