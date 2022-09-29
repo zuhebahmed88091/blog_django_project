77c7168f5108e11f2c7d9a713d006fb5bb064f7b
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-29 08:47:07 +0600

    feat: We aren't checking if the author of the post is the person trying to access this update page. In here we can update the post of another author but logged in as a different user and it's not ideal. To solve this in views.py of blog we have to import UserPassesTestMixin first and then add this to our update view inheritence. After that we can create a method called test func and that is a function that our UserPassesTestMixin will run in order to see if our user passes a certain test condition. So the function will like this def test_func(self): and we want to get the exact post that we're currently updating. The way that we can get this is using a method of the update view called get object like post = self.get_object(). This will get the post that we are currently trying to update. Now we can check to make sure that the current user is the author of the post. To do that it is like if self.request.user == post.author: return True else: return False.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
@@ -33,13 +33,20 @@ class PostCreateView(LoginRequiredMixin , CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin , UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
 
 
 
