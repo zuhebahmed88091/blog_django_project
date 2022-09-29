2d8708c4231976e5868e7278b5e8a5f37618f0d6
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-29 17:45:35 +0600

    feat: We want to paginate the home view.To do that in our views.py in blog at PostListView we have to add paginate_by = 2. And in our home.html we add

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 2
 
 class PostDetailView(DetailView):
     model = Post
