c71b433fa879530fd83b0e53b33741b542f8da10
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-29 20:14:03 +0600

    feat: In our home page the post section has a user name as a link. But after clicking it we get no result. Let's build a route that will display only the posts from

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,15 @@ from .views import  (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 from . import views
 
 urlpatterns = [
     # path("", views.home, name="blog-home"),
     path("", PostListView.as_view(), name="blog-home"),
+    path("user/<str:username>", UserPostListView.as_view(), name="user-posts"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/update", PostUpdateView.as_view(), name="post-update"),
