e44e4472d72af806ed0b8875c22b29ee4de6b7b1
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-29 08:30:14 +0600

    feat: Create a update view by that we can update the posts.Firstly in views.py we have to import UpdateView. Now we will have to create PostUpdateView class. Then we have to add a path to our url patterns to blog's urls'py.We have to import PostUpdateView and create a url pattern like path(post/<int:pk>/update, PostUpdateView.as_view(), name=post-update). And for template as we told before it will use post_form template that we created for the create view.

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView
 )
 from .models import Post
 # from django.http import HttpResponse
@@ -25,6 +26,14 @@ class PostDetailView(DetailView):
     model = Post
 
 class PostCreateView(LoginRequiredMixin , CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin , UpdateView):
     model = Post
     fields = ['title', 'content']
 
