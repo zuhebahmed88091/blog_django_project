205ac694333688d9effa3452885d46c86da03575
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-29 17:50:57 +0600

    fix: In views.py of blog we change paginate_by = 5. That will show 5 posts i a page

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 2
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
