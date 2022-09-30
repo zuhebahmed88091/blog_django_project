6ca07f9145e27e2c2160703470c772f22923c53d
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-09-30 11:48:18 +0600

    feat: We will need password_reset_email.html. That is a template that django is using in the background to create the email to send to the user so they can reset their password. When it's trying to create that email from the template it runs into a error because we don't have a route called password reset confirm and it also trying to pass 2 different things into the url parameters. Those are uidb64 and token.These are 2 parameters that we need to accept in our url so that we know that the user who requested the password reset  in urls.py of project we will create the route path(password-reset-confirm/<uidb64>/<token>/, auth_views.PasswordResetConfirmView.as_view(template_name=users/password_reset_confirm.html), name=password_reset_confirm). Now we will create the password_reset_confirm template in users. This is going to be form to actually reset our password.So only change will be in button which is named Reset Password.If we run the server it will tell connection refused error.Because we have no email server in

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,3 +128,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('USER_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('USER_PASS')
